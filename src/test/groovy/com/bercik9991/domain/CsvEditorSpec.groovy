e28073e7f09a45451beb6bafec30342992b5387b
am <bercik9991@gmail.com>
2021-08-16 13:38:35 +0200

    Created dummy test for CSV Editor.

---
+++ src/test/groovy/com/bercik9991/domain/CsvEditorSpec.groovy
@@ -0,0 +1,15 @@
+package com.bercik9991.domain
+
+import spock.lang.Specification
+
+/**
+ * @CsvEditorSpec specification defines conditions that
+ * the CSV Editor should meet to ensure
+ * correct data import, addition, edition, and export.
+ */
+class CsvEditorSpec extends Specification {
+    def 'should pass dummy CSV Editor spec'() {
+        expect:
+            true
+    }
+}
