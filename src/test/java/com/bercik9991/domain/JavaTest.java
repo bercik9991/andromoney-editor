af212d71ae13a3aeea104233669ffec4eec8fae6
am <bercik9991@gmail.com>
2021-08-16 10:39:09 +0200

    Renamed packages.

--- src/test/java/com/bercik9991/domain/JavaTest.java
+++ src/test/java/com/bercik9991/domain/JavaTest.java
@@ -1,12 +1,12 @@
-package junit;
+package com.bercik9991.domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class JavaTest {
 
-    @Test
-    void basicTest() {
-        Assertions.assertTrue(true);
-    }
+  @Test
+  void basicTest() {
+    Assertions.assertTrue(true);
+  }
 }
